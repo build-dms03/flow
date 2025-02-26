e4633879769c7f183a3b80cd1c30ba6102541af2
Dave S <build.dms03@gmail.com>
2025-02-26 10:05:19 -0800

    Project Flow - initial commit, with LibGDX preliminary setups

---
+++ android/src/main/java/bliss/flow/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package bliss.flow.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import bliss.flow.Core;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Core(), configuration);
+    }
+}
